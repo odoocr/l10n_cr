019bd6f03f0143603d99bf46cb419ed33fc5d052
Carlos Wong <cewongq@gmail.com>
2018-09-24 17:08:56 -0600

    Commit Initial

--- pos_electronic_invoice/static/src/js/pos_electronic_invoice.js
+++ pos_electronic_invoice/static/src/js/pos_electronic_invoice.js
@@ -5,21 +5,21 @@ odoo.define('pos_electronic_invoice', function(require){
 
 
     console.log("Pos js Loaded" , models)
-    screens.PaymentScreenWidget.include({
-        validate_order: function (force_validate) {
-            debugger;
-            var below_limit = this.pos.get_order().get_total_with_tax() <= this.pos.config.l10n_es_simplified_invoice_limit;
-            if (this.pos.config.iface_l10n_es_simplified_invoice) {
-                var order = this.pos.get_order();
-                if (below_limit) {
-                    order.set_simple_inv_number();
-                } else {
-                    // Force invoice above limit. Online is needed.
-                    order.to_invoice = true;
-                }
-            }
-            this._super(force_validate);
-        }
-    });
+    // screens.PaymentScreenWidget.include({
+    //     validate_order: function (force_validate) {
+    //         debugger;
+    //         var below_limit = this.pos.get_order().get_total_with_tax() <= this.pos.config.l10n_es_simplified_invoice_limit;
+    //         if (this.pos.config.iface_l10n_es_simplified_invoice) {
+    //             var order = this.pos.get_order();
+    //             if (below_limit) {
+    //                 order.set_simple_inv_number();
+    //             } else {
+    //                 // Force invoice above limit. Online is needed.
+    //                 order.to_invoice = true;
+    //             }
+    //         }
+    //         this._super(force_validate);
+    //     }
+    // });
 
 });
