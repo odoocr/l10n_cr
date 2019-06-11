00aa71ec71f655dd08ded82d40267e01508917c4
Richard Vong <rvong@indelsacr.com>
2019-06-10 23:56:57 -0600

    - Moved all county, district and neighborhood into XML files

--- l10n_cr_country_codes/models/country_codes.py
+++ l10n_cr_country_codes/models/country_codes.py
@@ -1,28 +1,30 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
+from odoo import models, fields
 
 
 class ResCountryCounty(models.Model):
     _name = "res.country.county"
+    _order = 'name'
 
-    code = fields.Char(string="Código", required=False, )
-    state_id = fields.Many2one(comodel_name="res.country.state", string="Provincia", required=False, )
-    name = fields.Char(string="Nombre", required=False, )
+    code = fields.Char(string="Código", required=True, )
+    state_id = fields.Many2one(comodel_name="res.country.state", string="Provincia", required=True)
+    name = fields.Char(string="Nombre", required=True, )
 
 
 class ResCountryDistrict(models.Model):
     _name = "res.country.district"
+    _order = 'name'
 
-    code = fields.Char(string="Código", required=False, )
-    county_id = fields.Many2one(comodel_name="res.country.county", string="Cantón", required=False, )
-    name = fields.Char(string="Nombre", required=False, )
+    code = fields.Char(string="Código", required=True, )
+    county_id = fields.Many2one(comodel_name="res.country.county", string="Cantón", required=True)
+    name = fields.Char(string="Nombre", required=True, )
 
 
 class ResCountryNeighborhood(models.Model):
     _name = "res.country.neighborhood"
+    _order = 'name'
 
-    code = fields.Char(string="Código", required=False, )
-    district_id = fields.Many2one(comodel_name="res.country.district", string="Distrito", required=False, )
-    name = fields.Char(string="Nombre", required=False, )
+    code = fields.Char(string="Código", required=True, )
+    district_id = fields.Many2one(comodel_name="res.country.district", string="Distrito", required=True)
+    name = fields.Char(string="Nombre", required=True, )
