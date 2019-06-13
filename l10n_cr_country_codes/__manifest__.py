0187f9ade8ee07603d19e972d5e992153c846dfe
Richard Vong <rvong@indelsacr.com>
2019-06-12 22:59:55 -0600

    - Update merge odoocr base

--- l10n_cr_country_codes/__manifest__.py
+++ l10n_cr_country_codes/__manifest__.py
@@ -7,21 +7,13 @@
     'license': 'AGPL-3',
     'website': 'https://www.delfixcr.com',
     'category': 'Account',
-    'description':'''Codigos Pais para Facturación electronica Costa Rica.''',
+    'description': '''Codigos Pais para Facturación electronica Costa Rica.''',
     'depends': ['base', 'account', 'product'],
-<<<<<<< HEAD
-    'data': ['views/country_codes_views.xml',
-             'data/res.country.state.xml',
-             'data/res.country.county.xml',
-             'data/res.country.district.xml',
-             'data/res.country.neighborhood.xml',
-=======
     'data': [
              'data/res.country.state.csv',
              'data/res.country.county.csv',
              'data/res.country.district.csv',
              'data/res.country.neighborhood.csv',
->>>>>>> ba2d0b3369c6b87a619abf1d387948690a8c1e7f
              'security/ir.model.access.csv',
              'views/country_codes_views.xml',
              ],
