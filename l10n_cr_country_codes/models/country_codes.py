37b5ff868c4605ecc58c018c5ee4e166520790ff
Richard Vong <rvong@indelsacr.com>
2019-06-17 01:32:51 -0600

    - Added l10n_cr migration code

--- l10n_cr_country_codes/models/country_codes.py
+++ l10n_cr_country_codes/models/country_codes.py
@@ -28,3 +28,22 @@ class ResCountryNeighborhood(models.Model):
     code = fields.Char(string="Código", required=True, )
     district_id = fields.Many2one(comodel_name="res.country.district", string="Distrito", required=True)
     name = fields.Char(string="Nombre", required=True, )
+
+
+class ResCountryState(models.Model):
+    _inherit = 'res.country.state'
+
+    def try_migrate_old_l10n_cr(self):
+        self.env.cr.execute(
+            "SELECT module FROM ir_model_data WHERE model=%s AND name='state_SJ'", (self._name)
+        )
+        if self.env.cr.fetchone() == 'l10n_cr':
+            self.env.cr.execute(
+                "UPDATE ir_model_data SET name=lower(name) WHERE model=%s AND module='l10n_cr'",
+                (self._name)
+            )
+            self.env.cr.execute(
+                "UPDATE ir_model_data SET module='l10n_cr_country_codes' "
+                "WHERE model=%s AMD module='l10n_cr'",
+                (self._name)
+            )
