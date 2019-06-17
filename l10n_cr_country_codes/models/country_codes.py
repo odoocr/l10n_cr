3e81e057e41e32c4c8a2355e70426e01b0450215
Richard V <rvong@indelsacr.com>
2019-06-17 12:45:03 -0600

    - Attempt to add migration script for old l10n_cr states into l10n_cr_country_codes.

--- l10n_cr_country_codes/models/country_codes.py
+++ l10n_cr_country_codes/models/country_codes.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields
+import logging
+
+
+_logger = logging.getLogger(__name__)
 
 
 class ResCountryCounty(models.Model):
     _name = "res.country.county"
+    _description = "Country State County Subdivision"
     _order = 'name'
 
     code = fields.Char(string="Código", required=True, )
@@ -14,6 +19,7 @@ class ResCountryCounty(models.Model):
 
 class ResCountryDistrict(models.Model):
     _name = "res.country.district"
+    _description = "Country State County District Subdivision"
     _order = 'name'
 
     code = fields.Char(string="Código", required=True, )
@@ -23,6 +29,7 @@ class ResCountryDistrict(models.Model):
 
 class ResCountryNeighborhood(models.Model):
     _name = "res.country.neighborhood"
+    _description = "Country State County District Neighborhood Subdivision"
     _order = 'name'
 
     code = fields.Char(string="Código", required=True, )
@@ -34,16 +41,18 @@ class ResCountryState(models.Model):
     _inherit = 'res.country.state'
 
     def try_migrate_old_l10n_cr(self):
+        _logger.info('Check if needed to migrate old l10n_cr states')
         self.env.cr.execute(
-            "SELECT module FROM ir_model_data WHERE model=%s AND name='state_SJ'", (self._name)
+            "SELECT module FROM ir_model_data WHERE model=%s AND name='state_SJ'", (self._name,)
         )
         if self.env.cr.fetchone() == 'l10n_cr':
+            _logger.info('Proceed to migrade old l10n_cr states')
             self.env.cr.execute(
                 "UPDATE ir_model_data SET name=lower(name) WHERE model=%s AND module='l10n_cr'",
-                (self._name)
+                (self._name,)
             )
             self.env.cr.execute(
                 "UPDATE ir_model_data SET module='l10n_cr_country_codes' "
                 "WHERE model=%s AMD module='l10n_cr'",
-                (self._name)
+                (self._name,)
             )
