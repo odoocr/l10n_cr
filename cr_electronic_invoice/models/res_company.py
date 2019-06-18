a1b8df17492d55577c9b0f39a1c55fb3bee58bfe
Richard V <rvong@indelsacr.com>
2019-06-17 21:19:57 -0600

    - Modified neighborhood repeated names, this is already wrong coming from Hacienda - Attempt to add code to automatically install the Comprobante Confirmation sequence

--- cr_electronic_invoice/models/res_company.py
+++ cr_electronic_invoice/models/res_company.py
@@ -4,6 +4,12 @@ from odoo import models, fields, api, _
 import logging
 _logger = logging.getLogger(__name__)
 
+_TIPOS_CONFIRMACION = (
+    # Provides listing of types of comprobante confirmations
+    ('CCE_sequence_id', 'cce'),
+    ('CPCE_sequence_id', 'cpe'),
+    ('RCE_sequence_id', 'rce'),
+)
 
 class CompanyElectronic(models.Model):
     _name = 'res.company'
@@ -18,9 +24,9 @@ class CompanyElectronic(models.Model):
     district_id = fields.Many2one(comodel_name="res.country.district", string="Distrito", required=False, )
     county_id = fields.Many2one(comodel_name="res.country.county", string="Cantón", required=False, )
     neighborhood_id = fields.Many2one(comodel_name="res.country.neighborhood", string="Barrios", required=False, )
-    frm_ws_identificador = fields.Char(string="Usuario de Factura Electrónica", required=False, )     
+    frm_ws_identificador = fields.Char(string="Usuario de Factura Electrónica", required=False, )
     frm_ws_password = fields.Char(string="Password de Factura Electrónica", required=False, )
-    
+
     frm_ws_ambiente = fields.Selection(
         selection=[('disabled', 'Deshabilitado'), ('api-stag', 'Pruebas'), ('api-prod', 'Producción'), ], string="Ambiente",
         required=True, default='disabled',
@@ -37,9 +43,48 @@ class CompanyElectronic(models.Model):
 
     sucursal_MR = fields.Integer(string="Sucursal para secuencias de MRs", required=False, default="1")
     terminal_MR = fields.Integer(string="Terminal para secuencias de MRs", required=False, default="1")
-    CCE_sequence_id = fields.Many2one('ir.sequence', string='Secuencia de Confirmación de Aceptación Comprobante Electrónico', 
-                                      readonly=False, copy=False)
-    CPCE_sequence_id = fields.Many2one('ir.sequence', string='Secuencia de Confirmación de Aceptación Parcial Comprobante Electrónico', 
-                                       readonly=False, copy=False)
-    RCE_sequence_id = fields.Many2one('ir.sequence', string='Secuencia de Rechazo Comprobante Electrónico', 
-                                      readonly=False, copy=False)
+    CCE_sequence_id = fields.Many2one(
+        'ir.sequence',
+        string='Secuencia de Confirmación de Aceptación Comprobante Electrónico',
+        readonly=False,
+        copy=False,
+        )
+    CPCE_sequence_id = fields.Many2one(
+        'ir.sequence',
+        string='Secuencia de Confirmación de Aceptación Parcial Comprobante Electrónico',
+        readonly=False, copy=False)
+    RCE_sequence_id = fields.Many2one(
+        'ir.sequence', string='Secuencia de Rechazo Comprobante Electrónico',
+        readonly=False, copy=False)
+
+    @api.model
+    def create(self, vals):
+        """ Try to automatically add the Comprobante Confirmation sequence to the company. It will
+            first try to search if an existing sequence exists with the same name of the company.
+            If not it will attempt to create and assign before storing. The sequence that is
+            created will be name with the following syntax: <CC type>-<company_name>, where CC-type
+            is either: cce, cpe, rce
+        """
+        new_comp = super(CompanyElectronic, self).create(vals)
+        company_subname = vals.get('name')
+        company_subname = company_subname.split(' ')[0].lower()
+        IrSequence = self.env['ir.sequence']
+        to_write = {}
+        _logger.info('Called create in CompanyElectronic')
+        for field, tipo in _TIPOS_CONFIRMACION:
+            if field not in vals:
+                seq_name = '{}-{}'.format(tipo, company_subname)
+                seq = IrSequence.search(('name', '=', seq_name), limit=1)
+                if not seq:
+                    seq = IrSequence.create({
+                        'name': seq_name,
+                        'implementation': 'statndard',
+                        'padding': 10,
+                        'use_date_range': False,
+                        'company_id': new_comp.id,
+                    })
+                to_write[field] = seq.id
+
+        if to_write:
+            new_comp.write(to_write)
+        return new_comp
