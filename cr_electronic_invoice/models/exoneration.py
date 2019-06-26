4b13d42608dcc49be261ba6a9383ce6d97d8e4cc
Carlos Monge <carlos.monge.contractor@akc.org>
2019-06-26 12:42:43 -0600

    modify model of exoneration to exted from fiscal position

--- cr_electronic_invoice/models/exoneration.py
+++ cr_electronic_invoice/models/exoneration.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-from odoo import models, fields, api, _
+from odoo import models, fields
 
 
 class Exoneration(models.Model):
-    _name = "exoneration"
-
-    name = fields.Char(string="Nombre", required=False, )
-    type = fields.Many2one(
-        comodel_name="aut.ex", string="Tipo Autorizacion/Exoneracion", required=True, )
+    _name = "account.fiscal.position"
+    _inherit = ['account.fiscal.position']
+    has_exoneration = fields.Boolean(string = "Posee exoneración", required=True)
+    type_exoneration = fields.Many2one(
+        comodel_name="aut.ex", string="Tipo Autorizacion", required=True, )
     exoneration_number = fields.Char(
         string="Número de exoneración", required=False, )
-    name_institution = fields.Char(
-        string="Nombre de institución", required=False, )
     date = fields.Date(string="Fecha", required=False, )
-    percentage_exoneration = fields.Float(
-        string="Porcentaje de exoneración", required=False, )
