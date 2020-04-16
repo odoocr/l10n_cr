0dfe046e08ddc685c41788fcd157cc2e30cfbbc1
Carlos Wong <cewongq@gmail.com>
2020-04-16 13:26:07 -0600

    [FIX] Load XML: payment_method and tipo_documento

--- cr_electronic_invoice/models/api_facturae.py
+++ cr_electronic_invoice/models/api_facturae.py
@@ -985,7 +985,7 @@ def load_xml_data(invoice, load_lines, account_id, product_id=False, analytic_ac
         invoice.invoice_date = invoice.date_issuance
         emisor = invoice_xml.xpath("inv:Emisor/inv:Identificacion/inv:Numero", namespaces=namespaces)[0].text
         receptor = invoice_xml.xpath("inv:Receptor/inv:Identificacion/inv:Numero", namespaces=namespaces)[0].text
-        invoice.payment_methods_id = invoice.env['payment.methods'].search([('sequence', '=', invoice_xml.xpath("inv:MedioPago", namespaces=namespaces)[0].text)], limit=1)
+        invoice.tipo_documento= False
 
         if receptor != invoice.company_id.vat:
             raise UserError('El receptor no corresponde con la compañía actual con identificación ' +
@@ -1006,6 +1006,12 @@ def load_xml_data(invoice, load_lines, account_id, product_id=False, analytic_ac
 
         if partner:
             invoice.partner_id = partner
+
+            payment_method_node = invoice_xml.xpath("inv:MedioPago", namespaces=namespaces)
+            if payment_method_node:
+                invoice.payment_methods_id = invoice.env['payment.methods'].search([('sequence', '=', payment_method_node[0].text)], limit=1)
+            else:
+                invoice.payment_methods_id = partner.payment_methods_id     
         else:
             raise UserError(_('The provider in the invoice does not exists. Please review it.'))
 
