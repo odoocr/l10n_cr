1a107030a19e9c04b0a58ea77fc9b19676e4ea6b
TheCloneMaster <the.clone.master@gmail.com>
2020-05-08 12:02:49 -0600

    [13.0] send_mail fixes

--- cr_electronic_invoice/extensions/mail_compose_message.py
+++ cr_electronic_invoice/extensions/mail_compose_message.py
@@ -9,7 +9,7 @@ class MailComposeMessage(models.TransientModel):
     def send_mail(self, auto_commit=False):
         context = self._context
         if context.get('mass_mark_invoice_as_sent') and \
-                context.get('default_model') == 'account.invoice':
+                context.get('default_model') == 'account.move':
             account_invoice = self.env['account.move']
             invoice_ids = context.get('active_ids')
             for invoice in account_invoice.browse(invoice_ids):
