16ea62da5642bea6fb34eb0f1eff8b890d3f1afd
Esteban Monge <estebanmonge@riseup.net>
2022-03-20 16:22:59 -0600

    more fixes to comply Randall rules

--- l10n_cr_hacienda_info_query/__manifest__.py
+++ l10n_cr_hacienda_info_query/__manifest__.py
@@ -1,18 +1,13 @@
 {
-        "name" : "Consultar Información de Clientes en Hacienda Costa Rica",
-        'version': '14.0.1.0.0',
-        "author" : "Odoo Community Association (OCA), Odoo CR, Factura Sempai, FSS Solutions",
-        'license': 'LGPL-3',
-        "website" : "https://github.com/odoocr/l10n_cr",
-        "category" : "API",
-        "summary": """Consultar Nombre de Clientes en Hacienda Costa Rica""",
-        "depends" : ['base','contacts', 'point_of_sale'],
-        "data" : [
-                'views/res_company.xml',
-                'views/assets.xml',
-                ],
-        'qweb':[
-                'static/src/xml/Screens/ClientListScreen/ClientDetailsEdit.xml',
-                ],
-        "installable": True
+    "name": "Consultar Información de Clientes en Hacienda Costa Rica",
+    "version": '14.0.1.0.0',
+    "author": "Odoo Community Association (OCA), Odoo CR, Factura Sempai, FSS Solutions",
+    "license": 'LGPL-3',
+    "website": "https://github.com/odoocr/l10n_cr",
+    "category": "API",
+    "summary": """Consultar Nombre de Clientes en Hacienda Costa Rica""",
+    "depends": ['base', 'contacts', 'point_of_sale'],
+    "data": ['views/res_company.xml', 'views/assets.xml'],
+    "qweb": ['static/src/xml/Screens/ClientListScreen/ClientDetailsEdit.xml'],
+    "installable": True
 }
