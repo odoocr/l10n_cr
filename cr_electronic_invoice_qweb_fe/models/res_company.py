469914c483c714361803f1e6a0253f87f9abefec
Norlan Ruiz <ruiznorlan@gmail.com>
2022-03-22 00:12:15 -0600

    [FIX] 14.0 cr_electronic_invoice_qweb_fe MQT 1

--- cr_electronic_invoice_qweb_fe/models/res_company.py
+++ cr_electronic_invoice_qweb_fe/models/res_company.py
@@ -1,19 +1,5 @@
 
-
-import json
-import requests
-import logging
-import re
-from odoo import models, fields, api, _
-from odoo.exceptions import UserError
-from odoo.tools.safe_eval import safe_eval
-import datetime
-import pytz
-import base64
-import xml.etree.ElementTree as ET
-
-
-_logger = logging.getLogger(__name__)
+from odoo import models, fields
 
 
 class ResCompany(models.Model):
@@ -21,4 +7,10 @@ class ResCompany(models.Model):
 
     html_bank_account1 = fields.Html(string="HTML Cuenta CRC")
     html_bank_account2 = fields.Html(string="HTML Cuenta USD")
-    cr_invoice_color = fields.Selection([('green','Green'),('sempai','Sempai'),('moresempai','More Sempai'),('blue', 'Blue'),('black', 'Black')],required=True,default='black')
+    cr_invoice_color = fields.Selection([('green', 'Green'),
+                                         ('sempai', 'Sempai'),
+                                         ('moresempai', 'More Sempai'),
+                                         ('blue', 'Blue'),
+                                         ('black', 'Black')],
+                                        required=True,
+                                        default='black')
