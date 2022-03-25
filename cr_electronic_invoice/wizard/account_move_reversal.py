d872edc872c02fbf0911c7bd134bb9e64858f02a
Norlan Ruiz <ruiznorlan@gmail.com>
2022-03-24 21:04:41 -0600

    [FIX] 14.0 _prepare_default_reversal method

--- cr_electronic_invoice/wizard/account_move_reversal.py
+++ cr_electronic_invoice/wizard/account_move_reversal.py
@@ -1,5 +1,5 @@
 
-from odoo import models, fields
+from odoo import models, fields, api
 
 
 class AccountMoveReversal(models.TransientModel):
@@ -7,35 +7,37 @@ class AccountMoveReversal(models.TransientModel):
     """
     Account move reversal wizard, it cancel an account move by reversing it.
     """
+
     reference_code_id = fields.Many2one("reference.code", string="Reference Code", required=True)
     reference_document_id = fields.Many2one("reference.document", string="Reference Document Id", required=True)
 
     def _prepare_default_reversal(self, move):
         default_values = super()._prepare_default_reversal(move)
-
-        if move.tipo_documento in ('FE', 'TE') and move.state_tributacion == 'rechazado':
-            type_override = 'out_invoice'
-            tipo_doc = move.tipo_documento
-        elif move.type == 'out_refund':
-            type_override = 'out_invoice'
-            tipo_doc = 'ND'
-        elif move.type == 'out_invoice':
-            type_override = 'out_refund'
-            tipo_doc = 'NC'
-        elif move.type == 'in_invoice':
-            type_override = 'in_refund'
-            tipo_doc = 'NC'
+        
+        # Se realiza de esta forma ya que se pueden revertir documentos de tipo "entry" (asientos contables), 
+        # y no es necesario completar la información electrónica en estos documentos
+        if move.move_type == 'entry':
+            return {**default_values}
         else:
-            tipo_doc = 'ND'
-            type_override = 'in_invoice'
+            tipo_doc = False
+            if move.tipo_documento in ('FE', 'TE') and move.state_tributacion == 'rechazado':
+                tipo_doc = 'NC'
+            elif move.move_type == 'out_refund':
+                tipo_doc = 'ND'
+            elif move.move_type == 'out_invoice':
+                tipo_doc = 'NC'
+            elif move.move_type == 'in_invoice':
+                tipo_doc = 'NC'
+            elif move.move_type == 'in_refund':
+                tipo_doc = 'ND'
 
-        fe_values = {'invoice_id': move.id,
-                     'type_override': type_override,
-                     'tipo_documento': tipo_doc,
-                     'reference_code_id': self.reference_code_id.id,
-                     'reference_document_id': self.reference_document_id.id,
-                     'economic_activity_id': move.economic_activity_id.id,
-                     'payment_methods_id': move.payment_methods_id.id,
-                     'state_tributacion': False}
+            fe_values = {'invoice_id': move.id,
+                        'tipo_documento': tipo_doc,
+                        'reference_code_id': self.reference_code_id.id,
+                        'reference_document_id': self.reference_document_id.id,
+                        'economic_activity_id': move.economic_activity_id.id,
+                        'payment_methods_id': move.payment_methods_id.id,
+                        'state_tributacion': False}
 
-        return {**default_values, **fe_values}
+            return {**default_values, **fe_values}
+    
