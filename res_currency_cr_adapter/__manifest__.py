078a7d631c111e0b361e104d0b3b89a9eacb8afa
Norlan Ruiz <ruiznorlan@gmail.com>
2022-03-27 21:31:39 -0600

    [FIX] 14.0 res_currency_cr_adapter MQT 3

--- res_currency_cr_adapter/__manifest__.py
+++ res_currency_cr_adapter/__manifest__.py
@@ -5,7 +5,7 @@
     'name': 'Costa Rica Currency Adapter',
     'version': '14.0.1.0.0',
     'category': 'Account',
-    'author': "Akurey S.A.",
+    'author': "Odoo CR, Akurey S.A.",
     'website': 'https://github.com/akurey/ak-odoo',
     'license': 'AGPL-3',
     'depends': [
