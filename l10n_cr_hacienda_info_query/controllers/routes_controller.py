9f155ce27e41d41badc72e81e1bcf0cc3db6c91a
Norlan Ruiz <ruiznorlan@gmail.com>
2022-04-12 19:02:10 -0600

    Update routes_controller.py

--- l10n_cr_hacienda_info_query/controllers/routes_controller.py
+++ l10n_cr_hacienda_info_query/controllers/routes_controller.py
@@ -1,23 +1,25 @@
 from datetime import datetime
 import json
 import requests
+import logging
 
 from odoo import http
 from odoo.http import request
+
+
+_logger = logging.getLogger(__name__)
 
 
 class ActualizarPosApi(http.Controller):
     # https://api.thunder.com.ve/control_rig/84:F3:EB:22:6E:D9
     @http.route(['/cedula/<vat>', ], type='http', auth="user", website=True)
     def index(self, vat):
-        set_param = request.env['ir.config_parameter'].sudo().set_param
-        get_param = request.env['ir.config_parameter'].sudo().get_param
 
-        url_base = get_param('url_base')
-        url_base_yo_contribuyo = get_param('url_base_yo_contribuyo')
-        usuario_yo_contribuyo = get_param('usuario_yo_contribuyo')
-        token_yo_contribuyo = get_param('token_yo_contribuyo')
+        company_id = http.request.env['res.company'].sudo().search([], limit=1)
 
+        url_base_yo_contribuyo = company_id.url_base_yo_contribuyo
+        usuario_yo_contribuyo = company_id.usuario_yo_contribuyo
+        token_yo_contribuyo = company_id.token_yo_contribuyo
         if url_base_yo_contribuyo and usuario_yo_contribuyo and token_yo_contribuyo:
             url_base_yo_contribuyo = url_base_yo_contribuyo.strip()
 
@@ -26,10 +28,9 @@ class ActualizarPosApi(http.Controller):
 
             end_point = url_base_yo_contribuyo + 'identificacion=' + vat
 
-            headers = {'access-user': usuario_yo_contribuyo, 'access-token': token_yo_contribuyo}
+            headers = {'access-user': usuario_yo_contribuyo, 'access-token': token_yo_contribuyo }
 
             peticion = requests.get(end_point, headers=headers, timeout=10)
-
             all_emails_yo_contribuyo = ''
 
             if peticion.status_code in (200, 202) and len(peticion._content) > 0:
@@ -38,6 +39,8 @@ class ActualizarPosApi(http.Controller):
                 for email_yo_contribuyo in emails_yo_contribuyo:
                     all_emails_yo_contribuyo = all_emails_yo_contribuyo + email_yo_contribuyo['Correo'] + ','
                 all_emails_yo_contribuyo = all_emails_yo_contribuyo[:-1]
+
+        url_base = company_id.url_base
 
         if url_base:
             # Elimina la barra al final de la URL para prevenir error al conectarse
@@ -53,12 +56,15 @@ class ActualizarPosApi(http.Controller):
             ultimo_mensaje = 'Fecha/Hora: ' + str(datetime.now()) + \
                              ', Codigo: ' + str(peticion.status_code) + \
                              ', Mensaje: ' + str(peticion._content.decode())
-            set_param('ultima_respuesta', ultimo_mensaje)
+
             # Respuesta de la API
             if peticion.status_code in (200, 202) and len(peticion._content) > 0:
                 contenido = json.loads(str(peticion._content, 'utf-8'))
 
+                request.env.company.ultima_respuesta = ultimo_mensaje
+
                 if 'nombre' in contenido:
+
                     identification_id = ''
                     res_partner = http.request.env['res.partner']
 
@@ -74,13 +80,25 @@ class ActualizarPosApi(http.Controller):
 
                     if 'identification_id' in res_partner._fields:
 
+                        id_type = http.request.env['identification.type']
+
                         if 'tipoIdentificacion' in contenido:
                             clasificacion = contenido.get('tipoIdentificacion')
                             # Cedula Fisica
-                            identification_id = request.env['identification.type'].search([('code',
-                                                                                            '=',
-                                                                                            clasificacion)],
-                                                                                          limit=1).id
+                            if clasificacion == '01':
+                                identification_id = id_type.search([('code', '=', '01')], limit=1).id
+                            # Cedula Juridica
+                            elif clasificacion == '02':
+                                identification_id = id_type.search([('code', '=', '02')], limit=1).id
+                            # Cedula Juridica
+                            elif clasificacion == '03':
+                                identification_id = id_type.search([('code', '=', '03')], limit=1).id
+                            # Cedula Juridica
+                            elif clasificacion == '04':
+                                identification_id = id_type.search([('code', '=', '04')], limit=1).id
+                            # Cedula Juridica
+                            elif clasificacion == '05':
+                                identification_id = id_type.search([('code', '=', '05')], limit=1).id
                     if contenido.get('nombre') is not None:
                         name = contenido.get('nombre')
                         if 'activity_id' in res_partner._fields:
@@ -91,3 +109,6 @@ class ActualizarPosApi(http.Controller):
                         else:
                             retorno = {"nombre": str(name)}
                         return '%s' % str(retorno).replace("'", "\"")
+
+            else:
+                request.env.company.ultima_respuesta = ultimo_mensaje
