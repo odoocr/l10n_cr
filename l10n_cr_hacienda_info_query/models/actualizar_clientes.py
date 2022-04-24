6192dbf639ef644c7c10a8d89a4026fc915bdb75
Esteban Monge <estebanmonge@riseup.net>
2022-04-24 11:42:12 -0500

    Fix to handle errors when a lot crackers attacks hacienda and the service is unavailable

--- l10n_cr_hacienda_info_query/models/actualizar_clientes.py
+++ l10n_cr_hacienda_info_query/models/actualizar_clientes.py
@@ -33,8 +33,6 @@ class res_partner(models.Model):
     _name = 'res.partner'
     _inherit = "res.partner"
 
-#    cedula = fields.Char(related="vat")
-
     def limpiar_cedula(self,vat):
         if vat:
             return ''.join(i for i in vat if i.isdigit())
@@ -43,7 +41,6 @@ class res_partner(models.Model):
     def onchange_cedula(self):
         self.name = ''
         self.identification_id = ''
-#        self.vat = self.cedula
 
     #Funcion ejecutada al haber un cambio en el campo vat(cedula)
     @api.onchange('vat')
@@ -68,16 +65,20 @@ class res_partner(models.Model):
 
                 headers = {'access-user': usuario_yo_contribuyo, 'access-token': token_yo_contribuyo }
 
-                peticion = requests.get(end_point, headers=headers, timeout=10)
-                all_emails_yo_contribuyo = ''
+                try:
+                    peticion = requests.get(end_point, headers=headers, timeout=10)
+                    all_emails_yo_contribuyo = ''
 
-                if peticion.status_code in (200, 202) and len(peticion._content) > 0:
-                    contenido = json.loads(str(peticion._content, 'utf-8'))
-                    emails_yo_contribuyo = contenido['Resultado']['Correos']
-                    for email_yo_contribuyo in emails_yo_contribuyo:
-                        all_emails_yo_contribuyo = all_emails_yo_contribuyo + email_yo_contribuyo['Correo'] + ','
-                    all_emails_yo_contribuyo = all_emails_yo_contribuyo[:-1]
-                    self.email = all_emails_yo_contribuyo 
+                    if peticion.status_code in (200, 202) and len(peticion._content) > 0:
+                        contenido = json.loads(str(peticion._content, 'utf-8'))
+                        emails_yo_contribuyo = contenido['Resultado']['Correos']
+                        for email_yo_contribuyo in emails_yo_contribuyo:
+                            all_emails_yo_contribuyo = all_emails_yo_contribuyo + email_yo_contribuyo['Correo'] + ','
+                        all_emails_yo_contribuyo = all_emails_yo_contribuyo[:-1]
+                        self.email = all_emails_yo_contribuyo
+
+                except:
+                    self.env.user.notify_default(message='The email query service is unavailable at this moment', title='Query service')
 
             #Valida que existan el campo url_base
             if url_base:
@@ -96,42 +97,41 @@ class res_partner(models.Model):
                             }
 
                 #Petición GET a la API
-                peticion = requests.get(end_point, headers=headers, timeout=10)
+                try:
+                    peticion = requests.get(end_point, headers=headers, timeout=10)
 
-                ultimo_mensaje = 'Fecha/Hora: ' + str(datetime.now()) + ', Codigo: ' + str(peticion.status_code) + ', Mensaje: ' + str(peticion._content.decode())
+                    ultimo_mensaje = 'Fecha/Hora: ' + str(datetime.now()) + ', Codigo: ' + str(peticion.status_code) + ', Mensaje: ' + str(peticion._content.decode())
 
-                #Respuesta de la API
-                if peticion.status_code in (200,202) and len(peticion._content) > 0:
-                    contenido = json.loads(str(peticion._content,'utf-8'))
-                    actividades = contenido.get('actividades')
-# Dejo un ejemplo de logger para futuros debugs
-#                    _logger.info(contenido)
-                    self.env.cr.execute("UPDATE  res_company SET ultima_respuesta='%s' WHERE id=%s" % (ultimo_mensaje,self.company_id.id))
+                    #Respuesta de la API
+                    if peticion.status_code in (200,202) and len(peticion._content) > 0:
+                        contenido = json.loads(str(peticion._content,'utf-8'))
+                        actividades = contenido.get('actividades')
+                        self.env.cr.execute("UPDATE  res_company SET ultima_respuesta='%s' WHERE id=%s" % (ultimo_mensaje,self.company_id.id))
+                        if 'nombre' in contenido:
+                            #Compatibilidad con FE
+                            if 'identification_id' in self._fields:
+                                if 'tipoIdentificacion' in contenido:
+                                    clasificacion = contenido.get('tipoIdentificacion')
+                                    if clasificacion == '01':#Cedula Fisicaclasificacion
+                                        self.identification_id = self.env['identification.type'].search([('code', '=', '01')], limit=1).id
+                                    elif clasificacion == '02':#Cedula Juridica
+                                        self.identification_id = self.env['identification.type'].search([('code', '=', '02')], limit=1).id
+                                    elif clasificacion == '03':#Cedula Juridica
+                                        self.identification_id = self.env['identification.type'].search([('code', '=', '03')], limit=1).id
+                                    elif clasificacion == '04':#Cedula Juridica
+                                        self.identification_id = self.env['identification.type'].search([('code', '=', '04')], limit=1).id
+                                    elif clasificacion == '05':#Cedula Juridica
+                                        self.identification_id = self.env['identification.type'].search([('code', '=', '05')], limit=1).id
 
-                    if 'nombre' in contenido:
+                            if contenido.get('nombre') != None:
+                                name = contenido.get('nombre')
+                                self.name = name
+                                for act in actividades:
+                                    if act.get('estado') == 'A':
+                                        self.activity_id = self.env['economic.activity'].search([('code', '=', str(act.get('codigo')))], limit=1).id
 
-                        #Compatibilidad con FE
-                        if 'identification_id' in self._fields:
-                            if 'tipoIdentificacion' in contenido:
-                                clasificacion = contenido.get('tipoIdentificacion')
-                                if clasificacion == '01':#Cedula Fisicaclasificacion
-                                    self.identification_id = self.env['identification.type'].search([('code', '=', '01')], limit=1).id
-                                elif clasificacion == '02':#Cedula Juridica
-                                    self.identification_id = self.env['identification.type'].search([('code', '=', '02')], limit=1).id
-                                elif clasificacion == '03':#Cedula Juridica
-                                    self.identification_id = self.env['identification.type'].search([('code', '=', '03')], limit=1).id
-                                elif clasificacion == '04':#Cedula Juridica
-                                    self.identification_id = self.env['identification.type'].search([('code', '=', '04')], limit=1).id
-                                elif clasificacion == '05':#Cedula Juridica
-                                    self.identification_id = self.env['identification.type'].search([('code', '=', '05')], limit=1).id
-
-                        if contenido.get('nombre') != None:
-                            name = contenido.get('nombre')
-                            self.name = name
-                            for act in actividades:
-                                if act.get('estado') == 'A':
-                                    self.activity_id = self.env['economic.activity'].search([('code', '=', str(act.get('codigo')))], limit=1).id
-
-                #Si la petición arroja error se almacena en el campo ultima_respuesta de res_company. Nota: se usa execute ya que el metodo por objeto no funciono
-                else:
-                    self.env.cr.execute("UPDATE  res_company SET ultima_respuesta='%s' WHERE id=%s" % (ultimo_mensaje,self.company_id.id))
+                    #Si la petición arroja error se almacena en el campo ultima_respuesta de res_company. Nota: se usa execute ya que el metodo por objeto no funciono
+                    else:
+                        self.env.cr.execute("UPDATE  res_company SET ultima_respuesta='%s' WHERE id=%s" % (ultimo_mensaje,self.company_id.id))
+                except:
+                    self.env.user.notify_default(message='The name query service is unavailable at this moment', title='Query service')
