6a4f9f70ded64fcd29a5bd2af3d237364d9dcc86
Norlan Ruiz <95722875+ruiznorlan@users.noreply.github.com>
2022-06-19 17:11:57 -0600

    [FIX] Python exception
    LN:26
    TypeError: unsupported operand type(s) for -: 'datetime.datetime' and 'datetime.date'

--- res_currency_cr_adapter/models/res_currency.py
+++ res_currency_cr_adapter/models/res_currency.py
@@ -23,7 +23,7 @@ class ResCurrency(models.Model):
     def action_create_missing_exchange_rates(self):
         currency_rate_obj = self.env['res.currency.rate']
         # A day is added to fix the loss of the current day
-        today = datetime.today()
+        today = datetime.today().date()
         last_day = today + timedelta(days=1)
 
         first_day = currency_rate_obj.search([
