bd307f0fa0a672699b0f3c4641c0b19c34895149
lmoram <44929825+lmoram@users.noreply.github.com>
2022-08-01 22:41:40 -0600

    [FIX] Clone POS order lines to NC
     NC in the POS backend deletes order lines

--- cr_electronic_invoice_pos/models/electronic_invoice.py
+++ cr_electronic_invoice_pos/models/electronic_invoice.py
@@ -245,7 +245,15 @@ class PosOrder(models.Model):
                 'amount_total': -order.amount_total,
                 'amount_paid': 0,
             })
-
+           
+            for line in order.lines:
+                clone_line = line.copy({
+                    'name': line.name + _(' REFUND'),
+                    'order_id': clone.id,
+                    'qty': -line.qty,
+                    'price_subtotal': -line.price_subtotal,
+                    'price_subtotal_incl': -line.price_subtotal_incl,
+                })            
             pos_order += clone
         return {
             'name': _('Return Products'),
