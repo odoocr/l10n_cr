af643449f6d25da93d549bf85aafe0f0d8800654
Ivan Porras <ivanporraslopez@gmail.com>
2022-08-16 18:12:57 -0500

    Point Of Sale removed from dependencies
    Why POS is instaled?

--- l10n_cr_hacienda_info_query/__manifest__.py
+++ l10n_cr_hacienda_info_query/__manifest__.py
@@ -7,7 +7,7 @@
     "category": "API",
     "summary": """Consultar Nombre de Clientes en Hacienda Costa Rica""",
     "description": """Actualización automática de nombre de clientes a partir del API de Hacienda""",
-    "depends": ['base', 'contacts', 'point_of_sale', 'base_setup', 'l10n_cr_country_codes'],
+    "depends": ['base', 'contacts', 'base_setup', 'l10n_cr_country_codes'],
     "data": [
         'views/res_config_settings_views.xml',
         'data/res_config_settings.xml'
