e7a1bc0f0377b54653498bb75155c724a9f726af
Norlan Ruiz <ruiznorlan@gmail.com>
2022-09-24 16:30:14 -0600

    [ADD] README.rst

--- l10n_cr_hacienda_info_query_pos/__manifest__.py
+++ l10n_cr_hacienda_info_query_pos/__manifest__.py
@@ -1,12 +1,12 @@
 {
-    "name": "Consultar Información de Clientes en Hacienda Costa Rica",
+    "name": "Consultar Información de Clientes en Hacienda Costa Rica - TPV",
     'version': '15.0.1.0.0',
     "author": "Odoo CR, Factura Sempai, FSS Solutions",
     'license': 'LGPL-3',
     "website": "https://github.com/odoocr/l10n_cr",
     "category": "API",
-    "summary": """Consultar Nombre de Clientes en Hacienda Costa Rica""",
-    "description": """Actualización automática de nombre de clientes a partir del API de Hacienda""",
+    "summary": """Consultar Nombre de Clientes en Hacienda Costa Rica - TPV""",
+    "description": """Actualización automática de nombre de clientes a partir del API de Hacienda - TPV""",
     "depends": ['base', 'contacts', 'point_of_sale', 'l10n_cr_hacienda_info_query'],
     'assets': {
         'point_of_sale.assets': [
